afd78007d5d3543f87ad1864420171b4e23dec61
arunpunithkumar3-a11y <arunpunithkumar3@gmail.com>
2026-07-21 17:12:33 +0530

    fix: explicit outDir dist in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
